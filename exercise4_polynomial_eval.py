c02f150880454c076980f939d5aa050398f1310e
Canning <yuenkinning221@gmail.com>
2026-02-11 22:24:03 +0100

    Add unitest in exercise 4

--- exercise4_polynomial_eval.py
+++ exercise4_polynomial_eval.py
@@ -1,4 +1,6 @@
-﻿def polynomial_eval(coeffs, x):
+﻿import unittest
+
+def polynomial_eval(coeffs, x):
     n = len(coeffs)
     
     if n == 0:
@@ -10,3 +12,19 @@
         result = (result * x) + coeffs[i]
         
     return result
+class TestPolynomialEval(unittest.TestCase):
+
+    def test_image_example(self):
+        self.assertEqual(polynomial_eval([3, -2, 0, 5], 2.0), 39.0)
+
+    def test_linear(self):
+        self.assertEqual(polynomial_eval([1, 2], 3.0), 7.0)
+
+    def test_empty(self):
+        self.assertEqual(polynomial_eval([], 10), 0)
+
+    def test_negative_x(self):
+        self.assertEqual(polynomial_eval([3, -2], -1), 5)
+
+if __name__ == '__main__':
+    unittest.main()
