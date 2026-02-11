14d0812d13228a5bbe8bde5e31e06c5b77825799
Canning <yuenkinning221@gmail.com>
2026-02-11 15:48:28 +0100

    Done for exercise 4

---
+++ exercise4_polynomial_eval.py
@@ -0,0 +1,12 @@
+﻿def polynomial_eval(coeffs, x):
+    n = len(coeffs)
+    
+    if n == 0:
+        return 0
+    
+    result = coeffs[n - 1]
+    
+    for i in range(n - 2, -1, -1):
+        result = (result * x) + coeffs[i]
+        
+    return result
