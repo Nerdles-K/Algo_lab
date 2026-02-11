c27264164284ec8ad5410e69a338c70e66ba4748
Canning <yuenkinning221@gmail.com>
2026-02-11 14:59:52 +0100

    Done for exercise 6

---
+++ exercise6_first_unique.py
@@ -0,0 +1,19 @@
+﻿def first_unique_char(s):
+    
+    if not s: 
+        return -1
+
+    freq = {}
+
+    for c in s:
+        if c in freq:
+            freq[c] += 1
+        else:
+            freq[c] = 1
+
+    for i in range(len(s)):
+        c = s[i]
+        if freq[c] == 1:
+            return i
+
+    return -1
